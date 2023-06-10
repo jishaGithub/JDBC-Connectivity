41071f95e8053d8e4de8c26a60376ec8b3dfd15b
jishaGithub <131222172+jishaGithub@users.noreply.github.com>
2023-06-10 12:50:09 -0400

    Update MySQLCustomerDAO.java

--- DbConnection/src/main/java/com/solvd/laba/db/dao/MySQLDAO/MySQLCustomerDAO.java
+++ DbConnection/src/main/java/com/solvd/laba/db/dao/MySQLDAO/MySQLCustomerDAO.java
@@ -6,15 +6,10 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class MySQLCustomerDAO extends AbstractDAO<Customer> {
-    public MySQLCustomerDAO() {
-        ConfigFileDAO.loadPropertyConfigFile();
-    }
-
     @Override
     public Boolean findById(int id1) {
         System.out.println("Finding record of ID:" + id1 + ".....");
-        try (Connection connection = ConfigFileDAO.getDataSource().getConnection()) {
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM customer where id=?");
+       try (PreparedStatement preparedStatement = getConnection().prepareStatement("SELECT * FROM customer where id=?")) {
             preparedStatement.setInt(1, id1);
             ResultSet result = preparedStatement.executeQuery();
             result.next();
@@ -39,8 +34,7 @@ public class MySQLCustomerDAO extends AbstractDAO<Customer> {
     public ArrayList<Customer> selectAll() {
         System.out.println("Displaying all the rows from customer table");
         ArrayList<Customer> customers = new ArrayList<>();
-        try (Connection connection = ConfigFileDAO.getDataSource().getConnection()) {
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM customer");
+        try (PreparedStatement preparedStatement = getConnection().prepareStatement("SELECT * FROM customer")) {
             ResultSet result = preparedStatement.executeQuery();
             while (result.next()) {
                 int id = result.getInt("id");
@@ -63,9 +57,8 @@ public class MySQLCustomerDAO extends AbstractDAO<Customer> {
 
     @Override
     public void addNewRow(Customer row) {
-        try (Connection connection = ConfigFileDAO.getDataSource().getConnection()) {
-            String insertValueStatement = "INSERT INTO customer(first_name,last_name,phone_number,email,age) VALUES (?,?,?,?,?)";
-            PreparedStatement preparedStatement = connection.prepareStatement(insertValueStatement);
+        String insertValueStatement = "INSERT INTO customer(first_name,last_name,phone_number,email,age) VALUES (?,?,?,?,?)";
+        try (PreparedStatement preparedStatement = getConnection().prepareStatement(insertValueStatement)) {
             preparedStatement.setString(1, row.getFirstName());
             preparedStatement.setString(2, row.getLastName());
             preparedStatement.setString(3, row.getPhoneNumber());
@@ -82,9 +75,8 @@ public class MySQLCustomerDAO extends AbstractDAO<Customer> {
 
     @Override
     public void update(Customer customer, int id) {
-        try (Connection connection = ConfigFileDAO.getDataSource().getConnection()) {
-            String updateStatement = "UPDATE customer SET first_name=?,last_name=?,phone_number=?,email=?,age=? WHERE id=?";
-            PreparedStatement preparedStatement = connection.prepareStatement(updateStatement);
+        String updateStatement = "UPDATE customer SET first_name=?,last_name=?,phone_number=?,email=?,age=? WHERE id=?";
+        try (PreparedStatement preparedStatement = getConnection().prepareStatement(updateStatement)) {
             preparedStatement.setString(1, customer.getFirstName());
             preparedStatement.setString(2, customer.getLastName());
             preparedStatement.setString(3, customer.getPhoneNumber());
@@ -102,10 +94,9 @@ public class MySQLCustomerDAO extends AbstractDAO<Customer> {
 
     @Override
     public void delete(int id) {
-        try (Connection connection = ConfigFileDAO.getDataSource().getConnection()) {
+        String deleteStatement = "DELETE from customer WHERE id=?";
+        try (PreparedStatement preparedStatement = getConnection().prepareStatement(deleteStatement)) {
             if (findById(id)) {
-                String deleteStatement = "DELETE from customer WHERE id=?";
-                PreparedStatement preparedStatement = connection.prepareStatement(deleteStatement);
                 preparedStatement.setInt(1, id);
                 preparedStatement.executeUpdate();
                 System.out.println("Deletion complete");
