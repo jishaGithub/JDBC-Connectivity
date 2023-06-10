59dde5cd83415431c5e0ed1459f203bd7fdc99f3
jishaGithub <131222172+jishaGithub@users.noreply.github.com>
2023-06-10 12:45:29 -0400

    Update MySQLBranchDAO.java

--- DbConnection/src/main/java/com/solvd/laba/db/dao/MySQLDAO/MySQLBranchDAO.java
+++ DbConnection/src/main/java/com/solvd/laba/db/dao/MySQLDAO/MySQLBranchDAO.java
@@ -6,18 +6,13 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class MySQLBranchDAO extends AbstractDAO<Branch> {
-    public MySQLBranchDAO() {
-        ConfigFileDAO.loadPropertyConfigFile();
-    }
-
     @Override
     public Boolean findById(int id1) {
         System.out.println("Finding record of ID:" + id1 + ".....");
-        try (Connection connection = ConfigFileDAO.getDataSource().getConnection()) {
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM branch where id=?");
+        try (PreparedStatement preparedStatement = getConnection().prepareStatement("SELECT * FROM branch where id=?")) {
             preparedStatement.setInt(1, id1);
             ResultSet result = preparedStatement.executeQuery();
-            if (result.next()) {
+            if(result.next()) {
                 int branchId = result.getInt("id");
                 String branchName = result.getString("branch_name");
                 int branchLocationId = result.getInt("branch_location_id");
@@ -40,8 +35,7 @@ public class MySQLBranchDAO extends AbstractDAO<Branch> {
     public ArrayList<Branch> selectAll() {
             System.out.println("Displaying all the rows from branch table");
             ArrayList<Branch> branches = new ArrayList<>();
-            try (Connection connection = ConfigFileDAO.getDataSource().getConnection()) {
-                PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM branch");
+            try (PreparedStatement preparedStatement = getConnection().prepareStatement("SELECT * FROM branch")) {
                 ResultSet result = preparedStatement.executeQuery();
                 while (result.next()) {
                     int branchId = result.getInt("id");
@@ -61,9 +55,8 @@ public class MySQLBranchDAO extends AbstractDAO<Branch> {
 
     @Override
     public void addNewRow(Branch row) {
-        try (Connection connection = ConfigFileDAO.getDataSource().getConnection()) {
-            String insertValueStatement = "INSERT INTO branch (branch_name, branch_location_id) VALUES (?, ?)";
-            PreparedStatement preparedStatement = connection.prepareStatement(insertValueStatement);
+        String insertValueStatement = "INSERT INTO branch (branch_name, branch_location_id) VALUES (?, ?)";
+        try (PreparedStatement preparedStatement = getConnection().prepareStatement(insertValueStatement)) {
             preparedStatement.setString(1, row.getBranchName());
             preparedStatement.setInt(2, row.getBranchLocationId());
             preparedStatement.executeUpdate();
@@ -78,9 +71,8 @@ public class MySQLBranchDAO extends AbstractDAO<Branch> {
 
     @Override
     public void update(Branch branch, int id) {
-        try (Connection connection = ConfigFileDAO.getDataSource().getConnection()) {
-            String updateStatement = "UPDATE branch SET branch_name=?, branch_location_id=? WHERE id=?";
-            PreparedStatement preparedStatement = connection.prepareStatement(updateStatement);
+        String updateStatement = "UPDATE branch SET branch_name=?, branch_location_id=? WHERE id=?";
+        try (PreparedStatement preparedStatement = getConnection().prepareStatement(updateStatement)) {
             preparedStatement.setString(1, branch.getBranchName());
             preparedStatement.setInt(2, branch.getBranchLocationId());
             preparedStatement.setInt(3, id);
@@ -95,10 +87,9 @@ public class MySQLBranchDAO extends AbstractDAO<Branch> {
 
     @Override
     public void delete(int id) {
-        try (Connection connection = ConfigFileDAO.getDataSource().getConnection()) {
+        String deleteStatement = "DELETE from branch WHERE id=?";
+        try (PreparedStatement preparedStatement = getConnection().prepareStatement(deleteStatement)) {
             if (findById(id)) {
-                String deleteStatement = "DELETE from branch WHERE id=?";
-                PreparedStatement preparedStatement = connection.prepareStatement(deleteStatement);
                 preparedStatement.setInt(1, id);
                 preparedStatement.executeUpdate();
                 System.out.println("Deletion complete");
