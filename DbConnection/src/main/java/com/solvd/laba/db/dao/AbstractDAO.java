0585596ccd5174d7818bf854c24454295ad1ed4d
jishaGithub <131222172+jishaGithub@users.noreply.github.com>
2023-06-10 12:43:19 -0400

    Update AbstractDAO.java

--- DbConnection/src/main/java/com/solvd/laba/db/dao/AbstractDAO.java
+++ DbConnection/src/main/java/com/solvd/laba/db/dao/AbstractDAO.java
@@ -1,8 +1,19 @@
 package com.solvd.laba.db.dao;
 
+import com.solvd.laba.db.connection.ConfigFileDAO;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public abstract class AbstractDAO<T> implements GenericDAO<T> {
+    protected AbstractDAO() {
+        ConfigFileDAO.loadPropertyConfigFile();
+    }
+
+    protected Connection getConnection() throws SQLException {
+        return ConfigFileDAO.getDataSource().getConnection();
+    }
+
     public abstract Boolean findById(int id);
     public abstract ArrayList<T> selectAll();
     public abstract void addNewRow(T t);
