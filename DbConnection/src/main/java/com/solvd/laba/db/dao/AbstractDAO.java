d3dff1a88a4a965289d0349ad723261668b8f8c3
jishaGithub <131222172+jishaGithub@users.noreply.github.com>
2023-06-13 19:08:11 -0400

    Updated AbstractDAO.java
    I updated AbstractDAO class. The AbstractDAO class now includes all the connection information and just loads the db configuration files for all the dao classes.

--- DbConnection/src/main/java/com/solvd/laba/db/dao/AbstractDAO.java
+++ DbConnection/src/main/java/com/solvd/laba/db/dao/AbstractDAO.java
@@ -1,11 +1,15 @@
 package com.solvd.laba.db.dao;
 
-import java.util.ArrayList;
+import com.solvd.laba.db.connection.ConfigFileDAO;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public abstract class AbstractDAO<T> implements GenericDAO<T> {
-    public abstract Boolean findById(int id);
-    public abstract ArrayList<T> selectAll();
-    public abstract void addNewRow(T t);
-    public abstract void update(T t,int id);
-    public abstract void delete(int id);
+    protected AbstractDAO() {
+        ConfigFileDAO.loadPropertyConfigFile();
+    }
+
+    protected Connection getConnection() throws SQLException {
+        return ConfigFileDAO.getDataSource().getConnection();
+    }
 }
