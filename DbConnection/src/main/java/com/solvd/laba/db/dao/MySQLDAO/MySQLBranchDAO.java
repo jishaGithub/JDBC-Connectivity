f647b0ad9b92a69c4bb7ffb091755d84e302230b
jishaGithub <131222172+jishaGithub@users.noreply.github.com>
2023-06-14 12:47:24 -0400

    Add files via upload

--- DbConnection/src/main/java/com/solvd/laba/db/dao/MySQLDAO/MySQLBranchDAO.java
+++ DbConnection/src/main/java/com/solvd/laba/db/dao/MySQLDAO/MySQLBranchDAO.java
@@ -2,110 +2,99 @@ package com.solvd.laba.db.dao.MySQLDAO;
 
 import com.solvd.laba.db.dao.AbstractDAO;
 import com.solvd.laba.db.model.Branch;
-import java.sql.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class MySQLBranchDAO extends AbstractDAO<Branch> {
-    public MySQLBranchDAO() {
-        ConfigFileDAO.loadPropertyConfigFile();
-    }
+    private static final Logger logger = LogManager.getLogger(MySQLBranchDAO.class);
+    private final static String GET_BY_ID = "SELECT * FROM branch WHERE id=?";
+    private final static String GET_ALL = "SELECT * FROM branch";
+    private final static String INSERT_VALUES = "INSERT INTO branch (branch_name, branch_location_id) VALUES (?, ?)";
+    private final static String UPDATE_VALUE = "UPDATE branch SET branch_name=?, branch_location_id=? WHERE id=?";
+    private final static String DELETE_VALUE = "DELETE FROM branch WHERE id=?";
 
     @Override
-    public Boolean findById(int id1) {
-        System.out.println("Finding record of ID:" + id1 + ".....");
-        try (Connection connection = ConfigFileDAO.getDataSource().getConnection()) {
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM branch where id=?");
-            preparedStatement.setInt(1, id1);
+    public Branch get(int id) {
+        logger.info("Finding record of ID: " + id + "...");
+        try (PreparedStatement preparedStatement = getConnection().prepareStatement(GET_BY_ID)) {
+            preparedStatement.setInt(1, id);
             ResultSet result = preparedStatement.executeQuery();
             if (result.next()) {
                 int branchId = result.getInt("id");
                 String branchName = result.getString("branch_name");
                 int branchLocationId = result.getInt("branch_location_id");
-                Branch branch = new Branch(branchId, branchName, branchLocationId);
-                System.out.println(branch);
-                System.out.println(" ");
-                return true;
+                return new Branch(branchId, branchName, branchLocationId);
             } else {
-                System.out.println("No record found. Invalid ID");
-                System.out.println();
+                logger.info("No record found. Invalid ID");
             }
         } catch (SQLException e) {
-            System.out.println("SQL query error");
+            logger.info("SQL query error");
             e.printStackTrace();
         }
-        return false;
+        return null;
     }
 
     @Override
-    public ArrayList<Branch> selectAll() {
-            System.out.println("Displaying all the rows from branch table");
-            ArrayList<Branch> branches = new ArrayList<>();
-            try (Connection connection = ConfigFileDAO.getDataSource().getConnection()) {
-                PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM branch");
-                ResultSet result = preparedStatement.executeQuery();
-                while (result.next()) {
-                    int branchId = result.getInt("id");
-                    String branchName = result.getString("branch_name");
-                    int branchLocationId = result.getInt("branch_location_id");
-                    Branch branch = new Branch(branchId,branchName, branchLocationId);
-                    branches.add(branch);
-                }
-                System.out.println(branches);
-                return branches;
-            } catch (SQLException e) {
-                System.out.println("Error");
-                e.printStackTrace();
+    public ArrayList<Branch> get() {
+        logger.info("Displaying all the rows from the branch table...");
+        ArrayList<Branch> branches = new ArrayList<>();
+        try (PreparedStatement preparedStatement = getConnection().prepareStatement(GET_ALL)) {
+            ResultSet result = preparedStatement.executeQuery();
+            while (result.next()) {
+                int branchId = result.getInt("id");
+                String branchName = result.getString("branch_name");
+                int branchLocationId = result.getInt("branch_location_id");
+                Branch branch = new Branch(branchId, branchName, branchLocationId);
+                branches.add(branch);
             }
-            return null;
+            logger.info(branches);
+            return branches;
+        } catch (SQLException e) {
+            logger.info("Error");
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @Override
-    public void addNewRow(Branch row) {
-        try (Connection connection = ConfigFileDAO.getDataSource().getConnection()) {
-            String insertValueStatement = "INSERT INTO branch (branch_name, branch_location_id) VALUES (?, ?)";
-            PreparedStatement preparedStatement = connection.prepareStatement(insertValueStatement);
+    public void add(Branch row) {
+        try (PreparedStatement preparedStatement = getConnection().prepareStatement(INSERT_VALUES)) {
             preparedStatement.setString(1, row.getBranchName());
             preparedStatement.setInt(2, row.getBranchLocationId());
             preparedStatement.executeUpdate();
-            System.out.println("Insertion complete");
-            System.out.println(selectAll());
+            logger.info("Insertion complete");
         } catch (SQLException e) {
-            System.out.println("Error");
+            logger.info("Error");
             e.printStackTrace();
         }
-
     }
 
     @Override
     public void update(Branch branch, int id) {
-        try (Connection connection = ConfigFileDAO.getDataSource().getConnection()) {
-            String updateStatement = "UPDATE branch SET branch_name=?, branch_location_id=? WHERE id=?";
-            PreparedStatement preparedStatement = connection.prepareStatement(updateStatement);
+        try (PreparedStatement preparedStatement = getConnection().prepareStatement(UPDATE_VALUE)) {
             preparedStatement.setString(1, branch.getBranchName());
             preparedStatement.setInt(2, branch.getBranchLocationId());
             preparedStatement.setInt(3, id);
             preparedStatement.executeUpdate();
-            System.out.println("Update complete");
-            System.out.println(selectAll());
+            logger.info("Update complete");
         } catch (SQLException e) {
-            System.out.println("Error");
+            logger.info("Error");
             e.printStackTrace();
         }
     }
 
     @Override
     public void delete(int id) {
-        try (Connection connection = ConfigFileDAO.getDataSource().getConnection()) {
-            if (findById(id)) {
-                String deleteStatement = "DELETE from branch WHERE id=?";
-                PreparedStatement preparedStatement = connection.prepareStatement(deleteStatement);
-                preparedStatement.setInt(1, id);
-                preparedStatement.executeUpdate();
-                System.out.println("Deletion complete");
-                System.out.println(selectAll());
-            }
+        try (PreparedStatement preparedStatement = getConnection().prepareStatement(DELETE_VALUE)) {
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+            logger.info("Deletion complete");
         } catch (SQLException e) {
-            System.out.println("Deletion unsuccessful!");
+            logger.info("Deletion unsuccessful!");
             e.printStackTrace();
         }
     }
