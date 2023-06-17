91b91701d6a4225f1384d11487693b800bb1e403
jishaGithub <131222172+jishaGithub@users.noreply.github.com>
2023-06-17 16:28:11 -0400

    Changes to Main

--- DbConnection/src/main/java/com/solvd/laba/Main.java
+++ DbConnection/src/main/java/com/solvd/laba/Main.java
@@ -1,54 +1,53 @@
 package com.solvd.laba;
 
-import com.solvd.laba.db.connection.ConfigFileDAO;
-import com.solvd.laba.db.dao.MySQLDAO.*;
+import com.solvd.laba.db.dao.*;
 import com.solvd.laba.db.model.CustomerAuthentication;
 import com.solvd.laba.db.model.Employee;
-import com.solvd.laba.db.service.MySQLService.*;
+import com.solvd.laba.db.service.*;
+import com.solvd.laba.db.util.ConfigFileDAO;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-        EmployeeService employeeService = new EmployeeService(new MySQLEmployeeDAO());
-        employeeService.findById(2);
-        employeeService.selectAll();
-        employeeService.updateRow(new Employee("Jacob","jacob@gmail.com",12),6);
-        employeeService.deleteRow(7);
-        BranchService branchService = new BranchService(new MySQLBranchDAO());
-        branchService.findById(3);
-        branchService.selectAll();
-        branchService.deleteRow(12);
-        CustomerService customerService = new CustomerService(new MySQLCustomerDAO());
-        customerService.findById(3);
-        customerService.selectAll();
-        CustomerAuthenticationService customerAuthenticationService = new CustomerAuthenticationService(new MySQLCustomerAuthenticationDAO());
-        customerAuthenticationService.selectAll();
-        customerAuthenticationService.updateRow(new CustomerAuthentication("jd2023","5634",4),4);
-        InsuranceService insuranceService = new InsuranceService(new MySQLInsuranceDAO());
-        insuranceService.selectAll();
-        insuranceService.deleteRow(6);
-        PaymentService paymentService = new PaymentService(new MySQLPaymentDAO());
-        paymentService.findById(3);
-        paymentService.selectAll();
-        paymentService.deleteRow(15);
-        PaymentMethodService paymentMethodService = new PaymentMethodService(new MySQLPaymentMethodDAO());
-        paymentMethodService.selectAll();
-        PromotionService promotionService = new PromotionService(new MySQLPromotionDAO());
-        promotionService.selectAll();
-        RentalService rentalService = new RentalService(new MySQLRentalDAO());
-        rentalService.selectAll();
-        RentalRateService rentalRateService = new RentalRateService(new MySQLRentalRateDAO());
-        rentalRateService.selectAll();
-        VehicleCategoryService vehicleCategoryService = new VehicleCategoryService(new MySQLVehicleCategoryDAO());
-        vehicleCategoryService.selectAll();
-        VehicleFeatureService vehicleFeatureService = new VehicleFeatureService(new MySQLVehicleFeatureDAO());
-        vehicleFeatureService.selectAll();
-        VehicleHasVehicleFeatureService vehicleHasVehicleFeatureService = new VehicleHasVehicleFeatureService(new MySQLVehicleHasVehicleFeatureDAO());
-        vehicleHasVehicleFeatureService.selectAll();
+        Logger logger = LogManager.getLogger(VehicleFeatureService.class);
+        EmployeeService employeeService = new EmployeeService(new EmployeeDAO());
+        employeeService.get(4);
+        employeeService.update(new Employee(15,"Sutton","sutton@gmail.com",13),15);
+        employeeService.delete(7);
+        BranchService branchService = new BranchService(new BranchDAO());
+        branchService.get(10);
+        branchService.delete(12);
+        CustomerService customerService = new CustomerService(new CustomerDAO());
+        customerService.get(17);
+        CustomerAuthenticationService customerAuthenticationService = new CustomerAuthenticationService(new CustomerAuthenticationDAO());
+        customerAuthenticationService.get();
+        customerAuthenticationService.update(new CustomerAuthentication(4, "jd2023", "5634", 4),4);
+        InsuranceService insuranceService = new InsuranceService(new InsuranceDAO());
+        insuranceService.delete(6);
+        PaymentService paymentService = new PaymentService(new PaymentDAO());
+        paymentService.get(3);
+        paymentService.delete(15);
+        PaymentMethodService paymentMethodService = new PaymentMethodService(new PaymentMethodDAO());
+        paymentMethodService.get();
+        PromotionService promotionService = new PromotionService(new PromotionDAO());
+        promotionService.get(2);
+        RentalService rentalService = new RentalService(new RentalDAO());
+        rentalService.get();
+        RentalRateService rentalRateService = new RentalRateService(new RentalRateDAO());
+        rentalRateService.get(5);
+        VehicleCategoryService vehicleCategoryService = new VehicleCategoryService(new VehicleCategoryDAO());
+        vehicleCategoryService.get(5);
+        VehicleFeatureService vehicleFeatureService = new VehicleFeatureService(new VehicleFeatureDAO());
+        vehicleFeatureService.get();
+        VehicleHasVehicleFeatureService vehicleHasVehicleFeatureService = new VehicleHasVehicleFeatureService(new VehicleHasVehicleFeatureDAO());
+        vehicleHasVehicleFeatureService.get(3);
         try {
             ConfigFileDAO.getDataSource().close();
         } catch (SQLException e) {
-            e.printStackTrace();
+            logger.info(e.getMessage());
         }
     }
 }
