95bd3220525a940f9963805958e2b8f618fdbaa6
jishaGithub <131222172+jishaGithub@users.noreply.github.com>
2023-06-23 14:13:14 -0400

    Update

--- DbConnection/src/main/java/com/solvd/laba/db/xml/jackson/JacksonParser.java
+++ DbConnection/src/main/java/com/solvd/laba/db/xml/jackson/JacksonParser.java
@@ -1,12 +1,14 @@
 package com.solvd.laba.db.xml.jackson;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.solvd.laba.db.model.*;
 import com.solvd.laba.db.xml.jaxb.CarRentalService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 public class JacksonParser {
     private static final Logger logger = LogManager.getLogger(JacksonParser.class);
@@ -20,8 +22,25 @@ public class JacksonParser {
 
         try (FileInputStream fileInputStream = new FileInputStream(jsonFile)) {
             ObjectMapper objectMapper = new ObjectMapper();
-            CarRentalService deSerializeJackson = objectMapper.readValue(fileInputStream, CarRentalService.class);
-            logger.info(deSerializeJackson);
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            objectMapper.setDateFormat(simpleDateFormat);
+            CarRentalService carRentalServiceJackson = objectMapper.readValue(fileInputStream, CarRentalService.class);
+            for (Customer customer : carRentalServiceJackson.getCustomers()) {
+                logger.info(customer);
+            }
+            for (CustomerAuthentication customerAuthentication : carRentalServiceJackson.getCustomerAuthentications()) {
+                logger.info(customerAuthentication);
+            }
+            for (Vehicle vehicle : carRentalServiceJackson.getVehicles()) {
+                logger.info(vehicle);
+            }
+            for (Rental rental : carRentalServiceJackson.getRentals()) {
+                String rentalAsString = objectMapper.writeValueAsString(rental);
+                logger.info(rentalAsString);
+            }
+            for (Employee employee : carRentalServiceJackson.getEmployees()) {
+                logger.info(employee);
+            }
         } catch (IOException e) {
             logger.error("Error occurred while parsing JSON: " + e.getMessage());
         }
